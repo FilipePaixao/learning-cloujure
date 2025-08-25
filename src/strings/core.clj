a6db9523946c470d9a2513b59e0ccef342d50989
Filipe Paixão <fpedrapaixao@gmail.com>
2025-08-25 20:35:54 -0300

    feat: improve comments and structure in core.clj

--- src/strings/core.clj
+++ src/strings/core.clj
@@ -3,7 +3,8 @@
 
 ;; def é como se fosse a declaração de uma variavel, porem com o valor imutavel
 (def saudacoes ;; Como se fosse uma variavel, mas declaro como uma funcao
-(str "Olá, " "Mundo"))
+(str "Olá, " "Mundo")) ;; str é usado para concatena e converte para string
 
-(defn -main [] 
-( println saudacoes ))
+(defn -main [] ;; função main que é executada assim que  rodarmos clj -M -m strings.core          
+( println saudacoes )) ;; println chama a função saudacoes e imprime seu valor
+
