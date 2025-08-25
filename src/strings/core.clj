7af119114156627e80c25e84da052d4d24eb1e04
Filipe Paixão <fpedrapaixao@gmail.com>
2025-08-25 20:28:32 -0300

    feat: add a new line to README for clarity

--- src/strings/core.clj
+++ src/strings/core.clj
@@ -2,7 +2,7 @@
 (ns strings.core)
 
 ;; def é como se fosse a declaração de uma variavel, porem com o valor imutavel
-(def saudacoes ;; Como se fosse uma variavel, mas declaro como uma funcao, faço a tipagem e declaro um valor
+(def saudacoes ;; Como se fosse uma variavel, mas declaro como uma funcao
 (str "Olá, " "Mundo"))
 
 (defn -main [] 
